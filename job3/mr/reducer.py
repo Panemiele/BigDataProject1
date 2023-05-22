fbf8538e46df7cdf01b8a082e712fa4191857c26
EMV93 <eros.volturo@hotmail.it>
2023-05-22 11:55:07 +0200

    Update reducer.py
    Update reducer

--- job3/mr/reducer.py
+++ job3/mr/reducer.py
@@ -25,15 +25,16 @@ groups = []
 for user in all_Users:
     group_added = False
     for group in groups:
-        common_products = user_Products[user].intersection(user_Products[group["user_id"][0]])
+        common_products = user_Products[user].intersection(group["common_products"])
         if len(common_products) >= 3:
-            group["user_id"].append(user)
-            group["common_products"].extend(list(common_products))
+            group["user_id"].add(user)
+            group["common_products"].update(common_products)
             group_added = True
             break
     if not group_added:
-        group = {"user_id": [user], "common_products": list(user_Products[user])}
+        group = {"user_id": {user}, "common_products": set(user_Products[user])}
         groups.append(group)
 
 for i, group in enumerate(groups, start=1):
-    print(f"Group {i}:", "Users_Id:", group["user_id"], "Products_Id:", list(set(group["common_products"])))
+    if len(group["user_id"])>1:
+        print(f"Group {i}:", "Users_Id:", group["user_id"], "Products_Id:", set(group["common_products"]))
